18fac3ae412e2ecefc91849bad8f544603cf3d5d
“[firstname <himakarhimmu@gmail.com>
2024-08-27 11:20:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+
+const PORT=3000||process.env.PORT;
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server Listening on PORT ${PORT}`);
+})
+
+
+
