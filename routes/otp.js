fef4b7bc698a882e736747794864166fcdba8b1b
IMinevitable-3 <komalsai130@gmail.com>
2024-08-30 11:16:25 +0530

    feature:otp service

--- routes/otp.js
+++ routes/otp.js
@@ -1,7 +1,15 @@
-const express=require("express");
-const router=express.Router();
-const {verifyEmail,verifyOtp}=require("../controllers/otp")
-router.post("/verifyemail",verifyEmail);
-router.post("/verifyotp",verifyOtp);
+const express = require("express");
+const router = express.Router();
+const {
+  requestOtp,
+  verifyOtp,
+  resendOtp,
+  checkOtpStatus,
+} = require("../controllers/otpService");
 
-module.exports=router;
+router.post("/request-otp", requestOtp);
+router.post("/verify-otp", verifyOtp);
+router.post("/resend-otp", resendOtp);
+router.get("/check-otp-status", checkOtpStatus);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-// import { PrismaClient } from '@prisma/client'
-const {PrismaClient} =require("@prisma/client")
-const prisma = new PrismaClient()
-const cors=require("cors");
-
-const express=require("express");
-const app=express();
-const authRoute=require("./routes/auth");
-const otpRoute=require("./routes/otp")
-const PORT=3000||process.env.PORT;
+const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const authRoute = require("./routes/auth");
+const otpService = require("./routes/otp");
+const PORT = 3000 || process.env.PORT;
+const app = express();
+const prisma = new PrismaClient();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
-app.use("/auth",authRoute);
-app.use("/otp",otpRoute);
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello</h1>")
-})
+app.use("/auth", authRoute);
+app.use("/otp", otpService);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server Listening on PORT ${PORT}`);
-})
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT ${PORT}`);
+});
